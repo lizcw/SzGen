445ea88c1078c007f9bab58c34b3e95bde69014d
Liz Cooper-Williams <Liz.Cooper-Williams@csiro.au>
2019-10-06 11:41:34 +1000

    Clinical lists now as lookup tables in admin

--- szgenapp/admin.py
+++ szgenapp/admin.py
@@ -2,8 +2,24 @@ from django.contrib import admin
 from szgenapp.models.samples import SampleType
 from szgenapp.models.participants import Country, Status
 from szgenapp.models.studies import StudyStatus
+from szgenapp.models.clinical import Dsmiv, Employment, EmploymentHistory, Gaf, Gafwl, Gender, Illness, Living, \
+    MaritalStatus, Onset,Religious,Severity, SeverityPattern, SymptomPattern
 # Register your models here.
 admin.site.register(SampleType)
 admin.site.register(Country)
 admin.site.register(Status)
 admin.site.register(StudyStatus)
+admin.site.register(Dsmiv)
+admin.site.register(Employment)
+admin.site.register(EmploymentHistory)
+admin.site.register(Gaf)
+admin.site.register(Gafwl)
+admin.site.register(Gender)
+admin.site.register(Illness)
+admin.site.register(Living)
+admin.site.register(MaritalStatus)
+admin.site.register(Onset)
+admin.site.register(Religious)
+admin.site.register(Severity)
+admin.site.register(SeverityPattern)
+admin.site.register(SymptomPattern)
--- szgenapp/models/clinical.py
+++ szgenapp/models/clinical.py
@@ -94,7 +94,8 @@ ONSET_CHOICES = (
     ('Abrupt', 'Abrupt (within a day)'),
     ('Acute', 'Acute (within a week)'),
     ('ModAcute', 'Moderately acute (within a month)'),
-    ('Gradual', 'Gradual (longer than a month)')
+    ('Gradual', 'Gradual (longer than a month)'),
+    ('Unknown', 'Unknown')
 )
 
 ILLNESS_CHOICES = (
@@ -127,6 +128,177 @@ CLINICAL_SUBTABLES = ['demographic', 'diagnosis', 'medicalhistory', 'symptomsgen
                       'symptomshallucination', 'symptomsbehaviour', 'symptomsdepression', 'symptomsmania']
 
 
+### Admin managed lookup lists
+class Gender(models.Model):
+    code = models.CharField(primary_key=True, max_length=3, blank=False, null=False, help_text='Gender code')
+    name = models.CharField(max_length=30, blank=False, null=False, help_text='Gender name')
+
+    class Meta:
+        verbose_name = 'Gender'
+        verbose_name_plural = 'Genders'
+
+    def __str__(self):
+        return '%s' % self.name
+
+
+class MaritalStatus(models.Model):
+    code = models.CharField(primary_key=True, max_length=30, help_text='Marital Status code')
+    name = models.CharField(max_length=100, blank=False, null=False, help_text='Marital Status name')
+
+    class Meta:
+        verbose_name = 'Marital Status'
+        verbose_name_plural = 'Marital Statuses'
+
+    def __str__(self):
+        return '%s' % self.name
+
+
+class Living(models.Model):
+    code = models.CharField(primary_key=True, max_length=30, help_text='Living code')
+    name = models.CharField(max_length=100, blank=False, null=False, help_text='Living name')
+
+    class Meta:
+        verbose_name = 'Living'
+        verbose_name_plural = 'Living options'
+
+    def __str__(self):
+        return '%s' % self.name
+
+
+class Employment(models.Model):
+    code = models.CharField(primary_key=True, max_length=30, help_text='Employment code')
+    name = models.CharField(max_length=100, blank=False, null=False, help_text='Employment name')
+
+    class Meta:
+        verbose_name = 'Employment'
+        verbose_name_plural = 'Employment options'
+
+    def __str__(self):
+        return '%s' % self.name
+
+
+class EmploymentHistory(models.Model):
+    code = models.SmallIntegerField(primary_key=True, help_text='Employment History code')
+    name = models.CharField(max_length=100, blank=False, null=False, help_text='Employment History name')
+
+    class Meta:
+        verbose_name = 'Employment History'
+        verbose_name_plural = 'Employment History options'
+
+    def __str__(self):
+        return '%s' % self.name
+
+
+class Dsmiv(models.Model):
+    code = models.CharField(primary_key=True, max_length=5, help_text='DSMIV code')
+    name = models.CharField(max_length=30, blank=False, null=False, help_text='DSMIV name')
+
+    class Meta:
+        verbose_name = 'DSMIV'
+        verbose_name_plural = 'DSMIV options'
+
+    def __str__(self):
+        return '%s' % self.name
+
+
+class Severity(models.Model):
+    code = models.CharField(primary_key=True, max_length=20, help_text='Severity code')
+    name = models.CharField(max_length=50, blank=False, null=False, help_text='Severity name')
+
+    class Meta:
+        verbose_name = 'Severity'
+        verbose_name_plural = 'Severity options'
+
+    def __str__(self):
+        return '%s' % self.name
+
+
+class SeverityPattern(models.Model):
+    code = models.SmallIntegerField(primary_key=True, help_text='Severity Pattern code')
+    name = models.CharField(max_length=100, blank=False, null=False, help_text='Severity Pattern name')
+
+    class Meta:
+        verbose_name = 'Severity Pattern'
+        verbose_name_plural = 'Severity Pattern options'
+
+    def __str__(self):
+        return '%s' % self.name
+
+
+class SymptomPattern(models.Model):
+    code = models.SmallIntegerField(primary_key=True, help_text='Symptom Pattern code')
+    name = models.CharField(max_length=100, blank=False, null=False, help_text='Symptom Pattern name')
+
+    class Meta:
+        verbose_name = 'Symptom Pattern'
+        verbose_name_plural = 'Symptom Pattern options'
+
+    def __str__(self):
+        return '%s' % self.name
+
+
+class Religious(models.Model):
+    code = models.CharField(primary_key=True, max_length=10, help_text='Religious code')
+    name = models.CharField(max_length=100, blank=False, null=False, help_text='Religious name')
+
+    class Meta:
+        verbose_name = 'Religious'
+        verbose_name_plural = 'Religious options'
+
+    def __str__(self):
+        return '%s' % self.name
+
+
+class Onset(models.Model):
+    code = models.CharField(primary_key=True, max_length=20, help_text='Onset code')
+    name = models.CharField(max_length=100, blank=False, null=False, help_text='Onset name')
+
+    class Meta:
+        verbose_name = 'Onset'
+        verbose_name_plural = 'Onset options'
+
+    def __str__(self):
+        return '%s' % self.name
+
+
+class Illness(models.Model):
+    code = models.SmallIntegerField(primary_key=True, help_text='Illness code')
+    name = models.CharField(max_length=100, blank=False, null=False, help_text='Illness name')
+
+    class Meta:
+        verbose_name = 'Illness'
+        verbose_name_plural = 'Illness options'
+
+    def __str__(self):
+        return '%s' % self.name
+
+
+class Gaf(models.Model):
+    code = models.CharField(primary_key=True, max_length=20, help_text='GAF code')
+    name = models.CharField(max_length=100, blank=False, null=False, help_text='GAF name')
+
+    class Meta:
+        verbose_name = 'GAF'
+        verbose_name_plural = 'GAF options'
+
+    def __str__(self):
+        return '%s' % self.name
+
+
+class Gafwl(models.Model):
+    code = models.CharField(primary_key=True, max_length=20, help_text='WL GAF code')
+    name = models.CharField(max_length=100, blank=False, null=False, help_text='WL GAF name')
+
+    class Meta:
+        verbose_name = 'WL GAF'
+        verbose_name_plural = 'WL GAF options'
+
+    def __str__(self):
+        return '%s' % self.name
+
+
+############################################################
+# Clinical models
 class Clinical(models.Model):
     """
     Clinical data per participant - subsectioned
@@ -217,21 +389,25 @@ class Demographic(models.Model):
     Clinically recorded demographic data
     """
     clinical = models.OneToOneField(Clinical, on_delete=models.CASCADE, primary_key=True)
-    gender = models.CharField(max_length=30, choices=GENDER_CHOICES, null=True, blank=True)
+    gender = models.ForeignKey(Gender, on_delete=models.SET_NULL, null=True, blank=True,
+                               help_text='Gender of participant')
     age_assessment = models.IntegerField(verbose_name='Age', validators=[validate_age],
                                          help_text='Age at the time of assessment', null=True, blank=True)
-    marital_status = models.CharField(choices=MARITAL_STATUS_CHOICES, max_length=30,
-                                      help_text='Marital status at the time of assessment', null=True, blank=True)
-    living_arr = models.CharField(choices=LIVING_CHOICES, max_length=30, verbose_name='Living arrangement',
-                                  help_text='Who the individual currently resides with (at time of assessment)',
-                                  null=True, blank=True)
+    marital_status = models.ForeignKey(MaritalStatus, on_delete=models.SET_NULL, null=True, blank=True,
+                                       help_text='Marital status at the time of assessment')
+    living_arr = models.ForeignKey(Living, on_delete=models.SET_NULL, null=True, blank=True,
+                                   verbose_name='Living arrangement',
+                                   help_text='Who the individual currently resides with (at time of assessment)'
+                                   )
     years_school = models.IntegerField(validators=[validate_school_years], help_text='Years of formal schooling',
                                        null=True, blank=True)
-    current_emp_status = models.CharField(choices=EMPLOYMENT_CHOICES, max_length=30, verbose_name='Current Employment',
-                                          help_text='Employment status (at time of assessment)', null=True, blank=True)
-    employment_history = models.SmallIntegerField(choices=EMPLOYMENT_HISTORY_CHOICES, verbose_name='Past Employment',
-                                                  help_text='Level of occupational disability over the past 5 years',
-                                                  null=True, blank=True)
+    current_emp_status = models.ForeignKey(Employment, on_delete=models.SET_NULL, null=True, blank=True,
+                                           verbose_name='Current Employment',
+                                           help_text='Employment status (at time of assessment)')
+    employment_history = models.ForeignKey(EmploymentHistory, on_delete=models.SET_NULL, null=True, blank=True,
+                                           verbose_name='Past Employment',
+                                           help_text='Level of occupational disability over the past 5 years'
+                                           )
 
 
 class Diagnosis(models.Model):
@@ -239,7 +415,8 @@ class Diagnosis(models.Model):
     Clinically recorded diagnosis
     """
     clinical = models.OneToOneField(Clinical, on_delete=models.CASCADE, primary_key=True)
-    summary = models.CharField(choices=DSMIV_CHOICES, max_length=30, null=True, blank=True, help_text='DSMIV Diagnosis')
+    summary = models.ForeignKey(Dsmiv, on_delete=models.SET_NULL, null=True, blank=True,
+                                verbose_name='DSMIV', help_text='DSMIV Diagnosis')
     age_onset = models.IntegerField(help_text='Age at onset of psychosis', validators=[validate_onset_age], null=True,
                                     blank=True)
     illness_duration = models.IntegerField(help_text='Illness duration (onset to current) in years',
@@ -327,25 +504,25 @@ class SymptomsGeneral(models.Model):
     1. General
     """
     clinical = models.OneToOneField(Clinical, on_delete=models.CASCADE, primary_key=True)
-    onset = models.CharField(max_length=20, choices=ONSET_CHOICES, null=True, blank=True,
+    onset = models.ForeignKey(Onset, on_delete=models.SET_NULL, null=True, blank=True,
                              verbose_name='Onset of Psychosis',
                              help_text='Rapidity of prodromal period (between noticeable social/occupational decline '
                                        'and definite onset of psychosis)')
-    severity_pattern = models.SmallIntegerField(choices=SEVERITY_PATTERN_CHOICES, null=True, blank=True,
+    severity_pattern = models.ForeignKey(SeverityPattern, on_delete=models.SET_NULL, null=True, blank=True,
                                                 verbose_name='Severity Pattern',
                                                 help_text='Pattern of severity of decline in functioning over the course '
                                                           'of the illness, as defined in the DIGS')
-    symptom_pattern = models.SmallIntegerField(choices=SYMPTOM_PATTERN_CHOICES, null=True, blank=True,
+    symptom_pattern = models.ForeignKey(SymptomPattern, on_delete=models.SET_NULL, null=True, blank=True,
                                                verbose_name='Symptom Pattern',
                                                help_text='Relationship between positive and negative symptoms throughout '
                                                          'the course of the illness, as defined in the DIGS')
-    illness_course = models.SmallIntegerField(choices=ILLNESS_CHOICES, null=True, blank=True,
+    illness_course = models.ForeignKey(Illness, on_delete=models.SET_NULL, null=True, blank=True,
                                               verbose_name='Illness Course',
                                               help_text='Categorical illness course, as defined in the DIGS')
-    curr_gaf = models.CharField(max_length=20, choices=GAF_CHOICES, null=True, blank=True,
+    curr_gaf = models.ForeignKey(Gaf, on_delete=models.SET_NULL, null=True, blank=True,
                                 verbose_name='Current Global Assessment of Function (GAF)',
                                 help_text='Current (past 30 days) rating on the Global Assessment of Function Scale')
-    wl_gaf = models.CharField(max_length=20, choices=WL_GAF_CHOICES, null=True, blank=True,
+    wl_gaf = models.ForeignKey(Gafwl, on_delete=models.SET_NULL, null=True, blank=True,
                               verbose_name='Worst Lifetime Global Assessment of Function (GAF)',
                               help_text='Lowest (lifetime) rating on the Global Assessment of Function Scale')
     current_ap_medication = models.CharField(max_length=10, choices=BOOLEAN_CHOICES, null=True, blank=True,
@@ -370,7 +547,8 @@ class SymptomsDelusion(models.Model):
     final_delusions = models.CharField(max_length=10, choices=BOOLEAN_CHOICES, null=True, blank=True,
                                        verbose_name='Final Delusions',
                                        help_text='Definite (lifetime) presence of delusions')
-    sevcur_delusions = models.CharField(max_length=20, choices=SEVERITY_CHOICES, null=True, blank=True,
+    sevcur_delusions = models.ForeignKey(Severity, on_delete=models.SET_NULL, null=True, blank=True,
+                                         related_name='sevcur_delusions',
                                         verbose_name='Severity of Current Delusions',
                                         help_text='Severity of current delusions (past 30 days)')
     bizarre_delusions = models.CharField(max_length=10, choices=BOOLEAN_CHOICES, null=True, blank=True,
@@ -399,7 +577,7 @@ class SymptomsDelusion(models.Model):
     grandiose_delusions = models.CharField(max_length=10, choices=BOOLEAN_CHOICES, null=True, blank=True,
                                            verbose_name='Grandiose Delusions',
                                            help_text='Presence (lifetime) of grandiose delusions')
-    religious_delusions = models.CharField(max_length=20, choices=RELIGIOUS_CHOICES, null=True, blank=True,
+    religious_delusions = models.ForeignKey(Religious, on_delete=models.SET_NULL, null=True, blank=True,
                                            verbose_name='Religious / Magic Delusions',
                                            help_text='Presence (lifetime) of religious delusions or delusions of magic')
     somatic_delusions = models.CharField(max_length=10, choices=BOOLEAN_CHOICES, null=True, blank=True,
@@ -422,7 +600,7 @@ class SymptomsHallucination(models.Model):
     final_hallucinations = models.CharField(max_length=10, choices=BOOLEAN_CHOICES, null=True, blank=True,
                                             verbose_name='Final Hallucinations',
                                             help_text='Definite (lifetime) presence of hallucinations')
-    severe_hallucinations = models.CharField(max_length=20, choices=SEVERITY_CHOICES, null=True, blank=True,
+    severe_hallucinations = models.ForeignKey(Severity, on_delete=models.SET_NULL, null=True, blank=True,
                                              verbose_name='Severity of Current Hallucinations',
                                              help_text="Severity of current hallucinations (past 30 days). "
                                                        "'Question' category usually grouped with "
@@ -457,7 +635,8 @@ class SymptomsBehaviour(models.Model):
                                      verbose_name='Disorganised Speech',
                                      help_text='Definite (lifetime) presence of disorganised speech/positive '
                                                'formal thought disorder')
-    severe_disorg_speech = models.CharField(max_length=20, choices=SEVERITY_CHOICES, null=True, blank=True,
+    severe_disorg_speech = models.ForeignKey(Severity, on_delete=models.SET_NULL, null=True, blank=True,
+                                             related_name='severe_disorg_speech',
                                             verbose_name='Severity of Current Disorganised Speech',
                                             help_text="Severity of current disorganised speech/positive formal "
                                                       "thought disorder (past 30 days). 'Question' category usually "
@@ -466,23 +645,29 @@ class SymptomsBehaviour(models.Model):
                                               verbose_name='Disorganised / Catatonic Behaviour',
                                               help_text='Definite (lifetime) presence of disorganised/catatonic '
                                                         'behaviour')
-    severe_disorg_catatonic_behav = models.CharField(max_length=20, choices=SEVERITY_CHOICES, null=True, blank=True,
+    severe_disorg_catatonic_behav = models.ForeignKey(Severity, on_delete=models.SET_NULL, null=True, blank=True,
+                                                      related_name='severe_disorg_catatonic_behav',
                                                      verbose_name='Severity of Current Disorganised Behaviour',
                                                      help_text='Severity of current disorganised/catatonic '
                                                                'behaviour (past 30 days)')
     negative_symptoms = models.CharField(max_length=10, choices=BOOLEAN_CHOICES, null=True, blank=True,
                                          verbose_name='Negative Symptoms',
                                          help_text='Definite (lifetime) presence of negative symptoms')
-    affective_flattening = models.CharField(max_length=20, choices=SEVERITY_CHOICES, null=True, blank=True,
+    affective_flattening = models.ForeignKey(Severity, on_delete=models.SET_NULL, null=True, blank=True,
+                                             related_name='affective_flattening',
                                             verbose_name='Affective Flattening',
                                             help_text='Severity of current affective flattening/inappropriate '
                                                       'affect (past 30 days)')
-    allogia = models.CharField(max_length=20, choices=SEVERITY_CHOICES, null=True, blank=True, verbose_name='Alogia',
+    allogia = models.ForeignKey(Severity, on_delete=models.SET_NULL, null=True, blank=True,
+                                related_name='allogia',
+                                verbose_name='Alogia',
                                help_text='Severity of current alogia/negative thought disorder (past 30 days)')
-    avolition = models.CharField(max_length=20, choices=SEVERITY_CHOICES, null=True, blank=True,
+    avolition = models.ForeignKey(Severity, on_delete=models.SET_NULL, null=True, blank=True,
+                                  related_name='avolition',
                                  verbose_name='Avolition / Apathy',
                                  help_text='Severity of current avolition/apathy (past 30 days)')
-    anhedonia = models.CharField(max_length=20, choices=SEVERITY_CHOICES, null=True, blank=True,
+    anhedonia = models.ForeignKey(Severity, on_delete=models.SET_NULL, null=True, blank=True,
+                                  related_name='anhedonia',
                                  verbose_name='Anhedonia / Asociality',
                                  help_text='Severity of current anhedonia/asociality (past 30 days)')
 
